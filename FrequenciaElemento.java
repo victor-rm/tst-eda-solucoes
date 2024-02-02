74dddbe1832e9285afd8ac590800075d23e4c3c4
Victor <victruu@outlook.com>
2024-02-01 21:09:55 -0300

    Adicionando FrequenciaElemento

---
+++ FrequenciaElemento.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/*
+ * Esse código possui uma complexidade linear, pois no pior caso ele será executado
+ *  n + 1 vezes, sendo n o tamanho do array
+ */
+
+class FrequenciaElemento {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // Entrada referente ao número a ser procurado
+        int numero_procurado = Integer.parseInt(sc.nextLine());
+        
+        // Segunda entrada, sequência de inteiros a serem observados
+        String[] inteiros =  sc.nextLine().split(" ");
+
+        sc.close();
+
+        int frequencia = 0;
+
+        // Verificação do numero a ser procurado na sequencia de inteiros disponibilizada.
+        for(int i = 0; i < inteiros.length; i++) {
+            if (Integer.parseInt(inteiros[i]) == numero_procurado ) {
+                frequencia++;
+            }
+        }
+        System.out.println(frequencia);
+    }
+}
