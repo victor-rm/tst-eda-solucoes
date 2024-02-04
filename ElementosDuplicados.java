facda9f086d6aabe341c79d08998dab205998f23
Victor <victruu@outlook.com>
2024-02-03 21:23:55 -0300

    Adicionando questão ElementosDuplicados

---
+++ ElementosDuplicados.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+// Complexidade quadrática (n**2), pois o algoritmo utiliza 2 loops
+
+class ElementosDuplicados {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Entrada responsável por receber os elementos.
+        String[] elementos = sc.nextLine().split(" ");
+
+        sc.close();
+
+        boolean temDuplicado = false;
+
+        // Loops para verificar se existem elementos duplicados
+        for (int i = 0; i < elementos.length; i++) {
+            for (int j = i + 1; j < elementos.length; j++) {
+                if (elementos[i].equals(elementos[j])) {
+                    temDuplicado = true;
+                }
+            }
+        }
+
+        System.out.println(temDuplicado);
+    }
+
+}
