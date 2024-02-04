8fb379a5ab978bab9923e6633bfa6c386be54247
Victor <victruu@outlook.com>
2024-02-04 19:35:38 -0300

    Adicionando questão EncontraPrimeiroNegativo

---
+++ EncontraPrimeiroNegativo.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class EncontraPrimeiroNegativo {
+
+    public static void main(String[] args) {
+        /**
+         * Classe de complexidade:
+         * Tendo em vista que ocorre um loop através da recursividade esse código escala de forma linear (n).
+         */
+        Scanner sc = new Scanner(System.in);
+
+        String[] elementos = sc.nextLine().split(" ");
+
+        System.out.println(primeiroNegativo(elementos, 0));
+
+    }
+
+    private static String primeiroNegativo(String[] elementos, int indice) {
+        if (Integer.parseInt(elementos[indice]) < 0) {
+            return elementos[indice];
+        } else {
+            if (indice < elementos.length - 1) {
+                return primeiroNegativo(elementos, indice + 1);
+            } else {
+                return "-";
+            }
+        }
+    }
+}
