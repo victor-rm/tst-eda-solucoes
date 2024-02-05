9be92d61fd28b0f6c9ce5607076d75b8395ca55b
123210384 VICTOR RIBEIRO MIRANDA <victor.ribeiro.miranda@lcc1-25.lcc.ufcg.edu.br>
2024-02-05 13:39:24 -0300

    Adicionando possível solução para a questão VetorCircular

--- VetorCircular.java
+++ VetorCircular.java
@@ -20,8 +20,27 @@ class VetorCircular {
         System.out.println(saida + circular(elementos, fatorcircular).trim());
 
     }
-
     private static String circular(String[] elementos, int fator) {
+        int resto = fator - elementos.length;
+        int limite = fator - elementos.length;
+        String vetor = "";
+        for(int i = 0; i < resto && limite != 0; i++) {
+            if (i == elementos.length) {
+                i = 0;
+                vetor += elementos[i] + " ";
+                limite--;
+            } else {
+                vetor += elementos[i] + " ";
+                limite--;
+            }
+        }
+        return vetor;
+    }
+}
+
+/* 
+    esse código não funciona para todos os casos, está aqui para fins de lembrança e compreensão
+    private static String circularAntigo(String[] elementos, int fator) {
         String vetor = "";
         for (int i = 0; i < elementos.length && fator - elementos.length >= elementos.length; i++) {
             if (i == elementos.length - 1) {
@@ -37,4 +56,4 @@ class VetorCircular {
         }
         return vetor;
     }
-}
+*/
